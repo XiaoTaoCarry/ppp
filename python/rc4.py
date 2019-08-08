e3af5cac773ac6fd77efbff8d4ad446047c8b2ab
wrk <wrk158@gmail.com>
2019-08-07 20:28:56 -0600

    send and receive

--- python/rc4.py
+++ python/rc4.py
@@ -1,6 +1,8 @@
 
 # -*- coding: utf-8 -*-
 
+from queue import Queue
+
 class RC4:
 
     def __init__(self, key):
@@ -58,6 +60,106 @@ class RC4:
         rtn = [ chr(x) for x in rtn]
         return ''.join(rtn)
     
+def hash(text):
+
+    digest = [0] * 16 
+    for i,x in enumerate(text):
+        digest[i%16] = digest[i%16] ^ ord(x)
+    
+    return digest
+
+key = 'test'
+packetQueue = Queue()
+rc4 = RC4(key)
+
+class Packet:
+    
+    def __init__(self, SC, data, type="default"):
+        
+        # 加密
+        text = []
+        for i in range(4):
+            text.append( chr(SC >> ((3-i))*8) )
+        for x in data:
+            text.append( x )
+
+        if type!="default" and len(data) < 252:     # 添0
+            numZero = 251 - len(data) 
+            text += chr(1)
+            for _ in range(numZero):
+                text += chr(0)
+
+        self.cipher = rc4.encrypt(''.join(text))
+        self.HV = hash(text)
+
+
+
+
+class Sender:
+
+    def __init__(self):
+        self.SC = 0
+
+    def sendMessage(self, message):
+        numSeg = len(message) // 252
+        for i in range(numSeg):
+            packet = Packet(self.SC, message[i*252 : (i+1)*252])
+            self.sendPacket(packet)
+        
+        packet = Packet(self.SC, message[ numSeg * 252 : ], type="last")
+        self.sendPacket(packet)
+        
+    def sendPacket(self, packet):
+        print("[sender] send a packet")
+        packetQueue.put(packet)
+        self.SC = self.SC + 1
+
+
+class Receiver:
+
+    def __init__(self):
+        self.SC = 0
+
+    def receivePacket(self):
+        print()
+        print("[receiver] receive a packet")
+        packet = packetQueue.get()
+
+        # 解密
+        message = rc4.decrypt(packet.cipher)
+        
+        # 获取SC 
+        sc = 0
+        for i,x in enumerate(message[0:4]):
+            sc = sc + ord(x) << ((3-i)*8)
+        print("[receiver] SC : " , sc)
+        if sc != self.SC:
+            print("the SC is not the same")
+            return None
+
+        # hash
+        text = list(message)
+        digest = hash(text)
+
+        for s, r in zip(packet.HV, digest):
+            if s!=r:
+                print("the hash value is not the same")
+                return None
+
+        self.SC = self.SC + 1
+
+        # 去掉0
+        lastIndex = -1
+        while message[lastIndex]==chr(0):
+            lastIndex -= 1
+        
+        print("[receiver] message : ")
+        if lastIndex != -1:
+            print(message[4:lastIndex])
+        elif message[lastIndex]==chr(1):
+            print(message[4:-1])
+        else:
+            print(message[4:])
 
 
 # usage:
@@ -69,10 +171,15 @@ class RC4:
 
 key = 'test'
 rc4 = RC4(key)
-m = "hello world"
-c = rc4.encrypt(m)
-print(c)
+m = ["helloWorld"] * 100
+m = ''.join(m)
 
-dm = rc4.decrypt(c) 
-print(dm)
+sender = Sender()
+receiver = Receiver()
 
+
+# 可以连续地发，也可以发一发，收一收
+sender.sendMessage(m)
+sender.sendMessage(m)
+while not packetQueue.empty():
+    receiver.receivePacket()
